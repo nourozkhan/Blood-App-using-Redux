e5b3ea0464d10fe98bf9219717c502cbade96391
nourozkhan <nouroz.khan1.nk@gmail.com>
2019-08-27 14:24:19 +0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,11 +3,11 @@ import firebase from 'firebase';
 
 
   var config = {
-    apiKey: "AIzaSyAiD2Zh1fLEove1F7OOLtHGu0fW0ST_JYU",
-    authDomain: "blood-bank-app-f403a.firebaseapp.com",
-    databaseURL: "https://blood-bank-app-f403a.firebaseio.com",
-    projectId: "blood-bank-app-f403a",
-    storageBucket: "blood-bank-app-f403a.appspot.com",
-    messagingSenderId: "587993381320"
+    apiKey: "**********************",
+    authDomain: "*************************",
+    databaseURL: "*************************",
+    projectId: "*************************",
+    storageBucket: "*************************",
+    messagingSenderId: "*************************"
   };
   firebase.initializeApp(config);
